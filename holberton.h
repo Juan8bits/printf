74c4097e9f3103289508d5ce849ebd6abb7dd4ec
SorenAcevedo <sorenacevedo@gmail.com>
2020-10-25 21:23:03 +0000

    Betty Correction holberton.c file

--- holberton.h
+++ holberton.h
@@ -5,25 +5,35 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
+/**
+* struct indicador - struct
+* @ind: type of data
+* @function: pointer to function
+* Description: select function
+*/
 typedef struct indicador
 {
 	char ind;
 	int (*function)(va_list);
-}ind_t;
-
+} ind_t;
+/**
+*
+*
+*
+*/
 typedef struct char_escape
 {
 	char c;
 	int ascii;
-}esc_t;
+} esc_t;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
 int ind_char(va_list listArg);
 int ind_string(va_list listArg);
-int ind_porcent();
-/*char *ind_integer(void);
-char *ind_integer(void);
-char *ind_undintege(void);*/
+int ind_porcent(void);
+/* char *ind_integer(void); */
+/*char *ind_integer(void); */
+/*char *ind_undintege(void);*/
 
 #endif
